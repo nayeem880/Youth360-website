0c57d63c64285433e3c965f940caaff9e9969743
nayeem <mhnayem289@gmail.com>
2020-12-23 22:45:06 +0600

    initial commit

---
+++ demo/ball.js
@@ -0,0 +1,49 @@
+jQuery(function($) {
+  'use strict';
+
+  $.exists = function(selector) {
+    return ($(selector).length > 0);
+  }
+  if ($.exists('#tb-ball-wrap')) {
+    // Some random colors
+    const colors = ['#3CC157', '#2AA7FF', '#FCBC0F', '#F85F36'];
+
+    const numBalls = 40;
+    const balls = [];
+
+    for (let i = 0; i < numBalls; i++) {
+      let ball = document.createElement('div');
+      ball.classList.add('tb-ball');
+      ball.style.background = colors[Math.floor(Math.random() * colors.length)];
+      ball.style.left = `${Math.floor(Math.random() * 100)}vw`;
+      ball.style.top = `${Math.floor(Math.random() * 100)}vh`;
+      ball.style.transform = `scale(${Math.random()})`;
+      ball.style.width = `${Math.random()}em`;
+      ball.style.height = ball.style.width;
+
+      balls.push(ball);
+      document.getElementById('tb-ball-wrap').append(ball);
+    }
+
+    // Keyframes
+    balls.forEach((el, i, ra) => {
+      let to = {
+        x: Math.random() * (i % 2 === 0 ? -11 : 11),
+        y: Math.random() * 12
+      };
+
+      let anim = el.animate(
+        [
+          { transform: 'translate(0, 0)' },
+          { transform: `translate(${to.x}rem, ${to.y}rem)` }
+        ], {
+          duration: (Math.random() + 1) * 2000, // random duration
+          direction: 'alternate',
+          fill: 'both',
+          iterations: Infinity,
+          easing: 'ease-in-out'
+        }
+      );
+    });
+  }
+});
